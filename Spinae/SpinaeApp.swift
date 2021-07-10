f58bdb05c6067b149a836dafb8f20a71d0c37558
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-10 13:58:57 +0900

    Initial Commit

---
+++ Spinae/SpinaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinaeApp.swift
+//  Spinae
+//
+//  Created by Yoshihiro Tanaka on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SpinaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
