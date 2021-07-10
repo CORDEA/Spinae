4b0b83c1c62765cfc0800d2adb5d7ba912d87af0
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-10 14:07:30 +0900

    add network extension

---
+++ SpinaeExtension/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SpinaeExtension
+//
+//  Created by Yoshihiro Tanaka on 7/10/21.
+//
+
+import Foundation
+import NetworkExtension
+
+autoreleasepool {
+    NEProvider.startSystemExtensionMode()
+}
+
+dispatchMain()
